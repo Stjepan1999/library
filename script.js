61deff96466c228fe9bc9d9e792502c0474f4d98
Stjepan1999 <106433830+Stjepan1999@users.noreply.github.com>
2023-10-18 17:36:45 +0200

    Fix duplicating book divs when adding new book

--- script.js
+++ script.js
@@ -7,19 +7,14 @@ function Book(title, author, pages, read) {
     this.read = read;
 }
 
-function addBookToLibrary(newBook) {
-    //let title = prompt("Book title: ");
-    //let author = prompt("Book author: ")
-    //let pages = prompt("Pages: ")
-    //let read = prompt("Have you read it?")
-
-    //let newBook = new Book(title, author, pages, read)
+function addBookToLibrary(title, author, pages, read) {
+    let newBook = new Book(title, author, pages, read)
     library.push(newBook)
 }
 
 
 function showBooks() {
-    
+    bookContainer.innerHTML = ""
     for (let i = 0; i < library.length; i++) {
         let bookDiv = document.createElement("div")
         bookDiv.classList.add("book-div")
@@ -52,7 +47,33 @@ bookDiv.classList.add("book-div")
 let firstBook = new Book("First book", "First author", 699, "true")
 let secondBook = new Book("Second book", "Second author", 699, "true")
 
+let addBookButton = document.getElementById("add-button")
 
+let bookShowed = null;
+
+
+addBookButton.addEventListener('click', () => {
+    let title = document.querySelector("[name='book-title']").value
+    let author = document.querySelector("[name='book-author']").value
+    let pages = document.querySelector("[name='book-pages']").value
+    let read = document.querySelector("[name='read-book']").value
+
+    addBookToLibrary(title, author, pages, read)
+    showBooks()
+
+    title.value = " "
+    author.value = " "
+    pages.value = " "
+
+    }
+    )
+
+let deleteAllButton = document.querySelector("#delete-all-button")
+
+deleteAllButton.addEventListener("click", () => {
+    library = []
+    showBooks()
+})
 
 
 showBooks()
