aac37bc944e0427157192d14e38df3f9c08d2f97
Stjepan1999 <106433830+Stjepan1999@users.noreply.github.com>
2023-10-19 19:42:56 +0200

    Add SVGs for book status info and create SVGs button  for changing status

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let library = [];
+let library = []; // Array for storing book objects
 
 function Book(title, author, pages, status) {
     this.title = title;
@@ -7,17 +7,24 @@ function Book(title, author, pages, status) {
     this.status = status;
 }
 
-function addBookToLibrary() {
+// Creating first book object for testing and pushing it to library array
+let atomicHabits = new Book("Atomic Habits", "James Clear", 243, "yes")
+let cantHurtMe = new Book("Can't hurt me", "David Goggings", 276, "no")
+library.push(atomicHabits)
+library.push(cantHurtMe)
 
+
+function addBookToLibrary() {
     let title = document.querySelector("[name='book-title']")
     let author = document.querySelector("[name='book-author']")
     let pages = document.querySelector("[name='book-pages']")
     let status = document.querySelector("[name='book-status']")
 
+    // yes = read it, no = haven't read it
     if (status.checked) {
-        status.value = "I read it"
+        status.value = "yes"
     } else {
-        status.value = "I haven't read it"
+        status.value = "no"
     }
 
     if (title.value && author.value && pages.value) {
@@ -36,9 +43,12 @@ function addBookToLibrary() {
 
 function showBooks() {
     bookContainer.innerHTML = ""
+
+    // Info section about book list
     let totalBooks = library.length;
     let booksRead = 0;
     let booksUnread = 0;
+
     for (let i = 0; i < library.length; i++) {
         let bookDiv = document.createElement("div")
         bookDiv.classList.add("book-div")
@@ -48,13 +58,44 @@ function showBooks() {
         authorDiv.textContent = library[i].author
         let pagesDiv = document.createElement("div")
         pagesDiv.textContent = library[i].pages
-        let statusDiv = document.createElement("div")
-        statusDiv.textContent = library[i].status
+
+        // SVG button for status
+        let statusDiv = document.createElement("button")
+        statusDiv.style.background = "var(--sidebar-color)"
+        statusDiv.style.border = "none";
+        
+        let icon = document.createElement("img")
+
+        if (library[i].status === "yes") {
+            icon.src = "./images/check-mark-icon.svg"
+            icon.style.fill = "#ff0000"
+        } else {
+            icon.src ="./images/x-symbol.svg"
+        }
+
+        icon.classList.add("status-button")
+        statusDiv.appendChild(icon)
+
+        statusDiv.addEventListener('click', () => {
+            if (library[i].status === "yes") {
+                library[i].status = "no";
+                icon.src ="./images/x-symbol.svg"
+            } else {
+                library[i].status = "yes";
+                icon.src = "./images/check-mark-icon.svg"
+            }
+
+            showBooks()
+        })
+        
+
+        // Delete button for each book
         let deleteButton = document.createElement("button")
         deleteButton.classList.add("delete-button")
         deleteButton.textContent = "DELETE BOOK"
         deleteButton.addEventListener('click', () => deleteBook(i))
 
+        // Creating book divs for each book in list
         bookContainer.appendChild(bookDiv)
         bookDiv.appendChild(titleDiv)
         bookDiv.appendChild(authorDiv)
@@ -63,12 +104,14 @@ function showBooks() {
         bookDiv.appendChild(deleteButton)
 
         //Count books
-        if (library[i].status === "I read it") {
+        if (library[i].status === "yes") {
             booksRead += 1
         } else {
             booksUnread += 1
         }
     }
+
+    // Updating info about books in list
     booksReadDiv.textContent = booksRead;
     booksUnreadDiv.textContent = booksUnread;
     totalBooksDiv.textContent = totalBooks;
@@ -77,16 +120,30 @@ function showBooks() {
     totalBooksContainer.appendChild(totalBooksDiv)
 }
 
+// Change status for each book
+function changeStatus(bookIndex) {
+    if (library[bookIndex].status === "yes") {
+        library[bookIndex].status = "no";
+        icon.src ="./images/x-symbol.svg"
+    } 
+}
+
+
+// Delete each book on button
 function deleteBook(bookIndex) {
     library.splice(bookIndex, 1);
     showBooks()
 } 
 
 
+
+// Adding class to books list divs
 let bookContainer = document.querySelector(".books-container")
 let bookDiv = document.createElement("div")
 bookDiv.classList.add("book-div")
 
+
+// Elements for info section above header
 let booksReadContainer = document.querySelector(".books-read");
 let booksUnreadContainer = document.querySelector(".books-unread");
 let totalBooksContainer = document.querySelector(".total-books")
@@ -95,21 +152,18 @@ let booksReadDiv = document.createElement("div")
 let booksUnreadDiv = document.createElement("div")
 let totalBooksDiv = document.createElement("div")
 
-booksReadDiv.textContent = 0
 
-booksReadContainer.appendChild(booksReadDiv)
-booksUnreadContainer.appendChild(booksUnreadDiv)
-totalBooksContainer.appendChild(totalBooksDiv)
-
-
+// Add book button
 let addBookButton = document.getElementById("add-button")
 addBookButton.addEventListener('click', () => addBookToLibrary())
 
-
+// Delete all button
 let deleteAllButton = document.querySelector(".delete-all-button")
 deleteAllButton.addEventListener("click", () => {
     library = [];
     showBooks();
 })
 
+
+// Showing book list at start
 showBooks()
